2f66ee1e05019c2d298d76eeb51dd110d729628b
daizhongyulog <1445504793@qq.com>
2022-01-13 11:05:32 +0800

    仓库初始化

--- mapper/SaleChanceMapper.java
+++
@@ -1,8 +0,0 @@
-package com.yjxxt.crm.mapper;
-
-import com.yjxxt.crm.base.BaseMapper;
-import com.yjxxt.crm.bean.SaleChance;
-
-public interface SaleChanceMapper extends BaseMapper<SaleChance,Integer> {
-
-}
